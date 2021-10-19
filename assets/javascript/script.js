eee5ee6ab9b417aeccf42cbbf2288ebb4faac674
udehsuccess <udehsuccess2@gmail.com>
2021-10-19 04:10:37 +0100

    work done

---
+++ assets/javascript/script.js
@@ -0,0 +1,23 @@
+;const showModal = (openButton, modalContent) => {
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
+
+    if(openBtn && modalContainer) {
+        openBtn.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+
+showModal('open-modal','modal-container')
+
+// Close
+
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.classList.remove('show-modal')
+}
+
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
